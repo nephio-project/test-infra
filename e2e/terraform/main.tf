da804cb205c349512d03daac3c29dbf4647093e9
Rado Chmiel <r.chmiel@partner.samsung.com>
2023-04-21 16:59:00 +0200

    adding output with IP address, removed VPC network dependency

--- e2e/terraform/main.tf
+++ e2e/terraform/main.tf
@@ -50,7 +50,4 @@ resource "google_compute_instance" "vm_instance" {
   }
 
 }
-resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
-  auto_create_subnetworks = "true"
-}
+
