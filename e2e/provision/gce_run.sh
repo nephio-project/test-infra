e72d508fbeaf104548b6080cd1410a2e3cfbf533
Victor Morales <v.morales@samsung.com>
2023-05-03 17:29:37 -0700

    Improve python package installation
    pip-tools allows to track dependencies on python packages, helping on the
    upgrade process. This change intruduces the pip-tools usage as well as improves
    the method to consume the python binaries through the pip installation.
    
    Signed-off-by: Victor Morales <v.morales@samsung.com>

--- e2e/provision/gce_run.sh
+++ e2e/provision/gce_run.sh
@@ -3,14 +3,13 @@ sudo apt-get clean
 sudo apt-get update
 yes | sudo  NEEDRESTART_SUSPEND=1  DEBIAN_FRONTEND=noninteractive apt-get install python3.10-venv python3-pip -y
 
-python3 -m venv venv
-venv/bin/pip3 install ansible
-venv/bin/pip3 install jmespath
-venv/bin/pip3 install pygithub
+python3 -m venv $HOME/.venv
+source $HOME/.venv/bin/activate
+pip install -r requirements.txt
 
-venv/bin/ansible-galaxy collection install community.general
-venv/bin/ansible-galaxy collection install kubernetes.docker
-venv/bin/ansible-galaxy collection install kubernetes.core
+ansible-galaxy collection install community.general
+ansible-galaxy collection install kubernetes.docker
+ansible-galaxy collection install kubernetes.core
 
 ssh-keygen -b 2048 -t rsa -f /home/ubuntu/.ssh/id_rsa -q -N ""
 cat /home/ubuntu/.ssh/id_rsa.pub >> /home/ubuntu/.ssh/authorized_keys
@@ -23,8 +22,8 @@ cp /home/ubuntu/nephio.yaml one-summit-22-workshop/nephio-ansible-install/invent
 
 cd one-summit-22-workshop/nephio-ansible-install
 
-../../venv/bin/ansible-playbook playbooks/install-prereq.yaml
-../../venv/bin/ansible-playbook playbooks/create-gitea.yaml
-../../venv/bin/ansible-playbook playbooks/create-gitea-repos.yaml
-../../venv/bin/ansible-playbook playbooks/deploy-clusters.yaml
-../../venv/bin/ansible-playbook playbooks/configure-nephio.yaml
+ansible-playbook playbooks/install-prereq.yaml
+ansible-playbook playbooks/create-gitea.yaml
+ansible-playbook playbooks/create-gitea-repos.yaml
+ansible-playbook playbooks/deploy-clusters.yaml
+ansible-playbook playbooks/configure-nephio.yaml
