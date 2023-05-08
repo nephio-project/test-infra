4665401c9504bc66fb6e054623ba15c880a8b3d1
Rado Chmiel <r.chmiel@partner.samsung.com>
2023-05-08 18:57:34 +0200

    Update gce_run.sh

--- e2e/provision/gce_run.sh
+++ e2e/provision/gce_run.sh
@@ -27,13 +27,13 @@ ansible-galaxy collection install -r galaxy-requirements.yml
 ssh-keygen -b 2048 -t rsa -f $HOME/.ssh/id_rsa -q -N ""
 cat $HOME/.ssh/id_rsa.pub >>$HOME/.ssh/authorized_keys
 
-apt-get -y install gcc-12
+sudo apt-get -y install gcc-12
 git clone -b v0.6.8 https://github.com/free5gc/gtp5g.git
 cd gtp5g
 make
-make install
-modprobe gtp5g
-dmesg | tail -n 4
+sudo make install
+sudo modprobe gtp5g
+sudo dmesg | tail -n 4
 
 git clone --depth 1 https://github.com/nephio-project/one-summit-22-workshop.git $HOME/workshop
 mkdir -p $HOME/workshop/nephio-ansible-install/inventory
