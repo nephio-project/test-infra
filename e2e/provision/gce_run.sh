4e4e94ac1bf827b890e26855856932815aad6bbb
Victor Morales <v.morales@samsung.com>
2023-05-25 09:12:01 -0700

    Add instructions to deploy free5gc workload (#48)
    Signed-off-by: Victor Morales <v.morales@samsung.com>

--- e2e/provision/gce_run.sh
+++ e2e/provision/gce_run.sh
@@ -11,7 +11,7 @@
 set -o pipefail
 set -o errexit
 set -o nounset
-[[ ${DEBUG:-false} != "true" ]] || set -o xtrace
+[[ "${DEBUG:-false}" != "true" ]] || set -o xtrace
 
 export HOME=${HOME:-/home/ubuntu/}
 
@@ -19,20 +19,21 @@ sudo apt-get clean
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install python3-venv python3-pip -y
 
-python3 -m venv $HOME/.venv
-source $HOME/.venv/bin/activate
+python3 -m venv "$HOME/.venv"
+# shellcheck disable=SC1091
+source "$HOME/.venv/bin/activate"
 pip install -r requirements.txt
 ansible-galaxy role install -r galaxy-requirements.yml
 ansible-galaxy collection install -r galaxy-requirements.yml
 
 rm -f ~/.ssh/id_rsa*
 echo -e "\n\n\n" | ssh-keygen -t rsa -N ""
-cat $HOME/.ssh/id_rsa.pub >>$HOME/.ssh/authorized_keys
+cat "$HOME/.ssh/id_rsa.pub" >>"$HOME/.ssh/authorized_keys"
 
 if ! lsmod | grep -q gtp5g; then
-    [[ -d $HOME/gtp5g ]] || git clone --depth 1 -b v0.6.8 https://github.com/free5gc/gtp5g.git $HOME/gtp5g
+    [[ -d "$HOME/gtp5g" ]] || git clone --depth 1 -b v0.6.8 https://github.com/free5gc/gtp5g.git "$HOME/gtp5g"
 
-    pushd $HOME/gtp5g >/dev/null
+    pushd "$HOME/gtp5g" >/dev/null
     if ! command -v gcc >/dev/null; then
         sudo apt-get update
         sudo apt-get -y install gcc
@@ -44,20 +45,31 @@ if ! lsmod | grep -q gtp5g; then
     popd >/dev/null
 fi
 
-[[ -d $HOME/workshop ]] || git clone --depth 1 https://github.com/nephio-project/one-summit-22-workshop.git $HOME/workshop
-mkdir -p $HOME/workshop/nephio-ansible-install/inventory
+if [ "${DEPLOYMENT_TYPE:-one-summit}" == "one-summit" ]; then
+    [[ -d "$HOME/workshop" ]] || git clone --depth 1 https://github.com/nephio-project/one-summit-22-workshop.git "$HOME/workshop"
+    mkdir -p "$HOME/workshop/nephio-ansible-install/inventory"
+    cp "$HOME/nephio.yaml" "$HOME/workshop/nephio-ansible-install/inventory/"
+    pushd "$HOME/workshop/nephio-ansible-install" >/dev/null
+    for playbook in install-prereq create-gitea create-gitea-repos deploy-clusters configure-nephio; do
+        if [[ ${DEBUG:-false} != "true" ]]; then
+            ansible-playbook "playbooks/$playbook.yaml"
+        else
+            ansible-playbook -vvv "playbooks/$playbook.yaml"
+        fi
+    done
+    popd >/dev/null
+else
+    # Management cluster creation
+    if [[ ${DEBUG:-false} != "true" ]]; then
+        ansible-playbook -i ~/nephio.yaml playbooks/cluster.yml
+    else
+        ansible-playbook -vvv -i ~/nephio.yaml playbooks/cluster.yml
+    fi
 
-cp $HOME/nephio.yaml $HOME/workshop/nephio-ansible-install/inventory/
-
-# Management cluster creation
-#if [[ ${DEBUG:-false} != "true" ]]; then
-#    ansible-playbook -i ~/nephio.yaml playbooks/cluster.yml
-#else
-#    ansible-playbook -vvv -i ~/nephio.yaml playbooks/cluster.yml
-#fi
-
-pushd $HOME/workshop/nephio-ansible-install >/dev/null
-for playbook in install-prereq create-gitea create-gitea-repos deploy-clusters configure-nephio; do
-    [[ ${DEBUG:-false} != "true" ]] && ansible-playbook "playbooks/$playbook.yaml" || ansible-playbook -vvv "playbooks/$playbook.yaml"
-done
-popd >/dev/null
+    # Deploy free5GC workload
+    if [[ ${DEBUG:-false} != "true" ]]; then
+        ansible-playbook -i ~/nephio.yaml playbooks/free5gc.yml
+    else
+        ansible-playbook -vvv -i ~/nephio.yaml playbooks/free5gc.yml
+    fi
+fi
