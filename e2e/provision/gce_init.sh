559ccf28f969f16d4e895a964c615d5f7e9e06d8
John Belamaric <jbelamaric@google.com>
2023-06-05 19:08:05 -0700

    Fix issue when branch name conflicts with file name

--- e2e/provision/gce_init.sh
+++ e2e/provision/gce_init.sh
@@ -36,7 +36,7 @@ apt-get install -y git
 cd /home/ubuntu
 
 runuser -u ubuntu git clone "$REPO" test-infra
-cd test-infra && runuser -u ubuntu git checkout "$BRANCH" && cd ..
+cd test-infra && runuser -u ubuntu git checkout -b "$BRANCH" --track "origin/$BRANCH" && cd ..
 
 sed -e "s/vagrant/ubuntu/" < /home/ubuntu/test-infra/e2e/provision/nephio.yaml > /home/ubuntu/nephio.yaml
 cd ./test-infra/e2e/provision
