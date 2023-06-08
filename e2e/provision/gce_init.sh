77c0a8f1f2d03c3a04c17b3429dbfd4753af915a
John Belamaric <jbelamaric@google.com>
2023-06-08 13:37:09 -0700

    gce_init.sh runs as root, fix unbound var USER

--- e2e/provision/gce_init.sh
+++ e2e/provision/gce_init.sh
@@ -27,6 +27,7 @@ DEPLOYMENT_TYPE=$(get_metadata nephio-setup-type "r1")
 RUN_E2E=$(get_metadata nephio-run-e2e "false")
 REPO=$(get_metadata nephio-test-infra-repo "https://github.com/nephio-project/test-infra.git")
 BRANCH=$(get_metadata nephio-test-infra-branch "main")
+NEPHIO_USER=${USER:-ubuntu}
 
 echo "$DEBUG, $DEPLOYMENT_TYPE, $RUN_E2E, $REPO, $BRANCH"
 
@@ -34,29 +35,29 @@ apt-get update
 apt-get install -y git
 
 
-cd /home/ubuntu
+cd /home/$NEPHIO_USER
 
 cat > .bash_aliases <<EOF
 alias k=kubectl
 EOF
 
-chown ubuntu:ubuntu .bash_aliases
+chown $NEPHIO_USER:$NEPHIO_USER .bash_aliases
 
-runuser -u ubuntu git clone "$REPO" test-infra
+runuser -u $NEPHIO_USER git clone "$REPO" test-infra
 if [[ "$BRANCH" != "main" ]]; then
-  cd test-infra && runuser -u ubuntu -- git checkout -b "$BRANCH" --track "origin/$BRANCH" && cd ..
+  cd test-infra && runuser -u $NEPHIO_USER -- git checkout -b "$BRANCH" --track "origin/$BRANCH" && cd ..
 fi
 
-sed -e "s/vagrant/ubuntu/" < /home/ubuntu/test-infra/e2e/provision/nephio.yaml > /home/ubuntu/nephio.yaml
+sed -e "s/vagrant/$NEPHIO_USER/" < /home/$NEPHIO_USER/test-infra/e2e/provision/nephio.yaml > /home/$NEPHIO_USER/nephio.yaml
 cd ./test-infra/e2e/provision
 export DEBUG DEPLOYMENT_TYPE
-runuser -u ubuntu ./gce_install_sandbox.sh
+runuser -u $NEPHIO_USER ./gce_install_sandbox.sh
 
 # Grant Docker permissions to current user
-if ! getent group docker | grep -q "$USER"; then
-    sudo usermod -aG docker "$USER"
+if ! getent group docker | grep -q "$NEPHIO_USER"; then
+    sudo usermod -aG docker "$NEPHIO_USER"
 fi
 
 if [[ "$RUN_E2E" == "true" ]]; then
-  runuser -u ubuntu ../e2e.sh
+  runuser -u $NEPHIO_USER ../e2e.sh
 fi
