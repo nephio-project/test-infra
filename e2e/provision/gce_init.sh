d9c697b27174e3f40a81ab1adb6ea123174f065e
liamfallon <liam.fallon@est.tech>
2023-06-12 15:08:20 +0100

    Updated init script to allow metadata override

--- e2e/provision/gce_init.sh
+++ e2e/provision/gce_init.sh
@@ -19,24 +19,22 @@ function get_metadata {
   echo $(curl -sf "http://metadata.google.internal/computeMetadata/v1/instance/attributes/$md" -H "Metadata-Flavor: Google" || echo "$df")
 }
 
-DEBUG=$(get_metadata nephio-setup-debug "false")
+DEBUG=${NEPHIO_DEBUG:-$(get_metadata nephio-setup-debug "false")}
 
 [[ "$DEBUG" != "true" ]] || set -o xtrace
 
-DEPLOYMENT_TYPE=$(get_metadata nephio-setup-type "r1")
-RUN_E2E=$(get_metadata nephio-run-e2e "false")
-REPO=$(get_metadata nephio-test-infra-repo "https://github.com/nephio-project/test-infra.git")
-BRANCH=$(get_metadata nephio-test-infra-branch "main")
-NEPHIO_USER=${USER:-ubuntu}
+DEPLOYMENT_TYPE=${NEPHIO_DEPLOYMENT_TYPE:-$(get_metadata nephio-setup-type "r1")}
+RUN_E2E=${NEPHIO_RUN_E2E:-$(get_metadata nephio-run-e2e "false")}
+REPO=${NEPHIO_REPO:-$(get_metadata nephio-test-infra-repo "https://github.com/nephio-project/test-infra.git")}
+BRANCH=${NEPHIO_BRANCH:-$(get_metadata nephio-test-infra-branch "main")}
+NEPHIO_USER=${NEPHIO_USER:-ubuntu}
 
-echo "$DEBUG, $DEPLOYMENT_TYPE, $RUN_E2E, $REPO, $BRANCH"
+echo "$DEBUG, $DEPLOYMENT_TYPE, $RUN_E2E, $REPO, $BRANCH, $NEPHIO_USER"
 
 apt-get update
 apt-get install -y git
 
-
 cd /home/$NEPHIO_USER
-
 
 runuser -u $NEPHIO_USER git clone "$REPO" test-infra
 if [[ "$BRANCH" != "main" ]]; then
@@ -49,13 +47,17 @@ chown $NEPHIO_USER:$NEPHIO_USER /home/$NEPHIO_USER/.bash_aliases
 sed -e "s/vagrant/$NEPHIO_USER/" < /home/$NEPHIO_USER/test-infra/e2e/provision/nephio.yaml > /home/$NEPHIO_USER/nephio.yaml
 cd ./test-infra/e2e/provision
 export DEBUG DEPLOYMENT_TYPE
-runuser -u $NEPHIO_USER ./gce_install_sandbox.sh
+
+if ! getent group docker > /dev/null; then
+    addgroup docker
+fi
 
 # Grant Docker permissions to current user
 if ! getent group docker | grep -q "$NEPHIO_USER"; then
     sudo usermod -aG docker "$NEPHIO_USER"
-    newgrp docker
 fi
+
+runuser -u $NEPHIO_USER ./gce_install_sandbox.sh
 
 if [[ "$RUN_E2E" == "true" ]]; then
   runuser -u $NEPHIO_USER ../e2e.sh
--- e2e/provision/gce_install_sandbox.sh
+++ e2e/provision/gce_install_sandbox.sh
@@ -22,8 +22,7 @@ function deploy_kpt_pkg {
   local temp=$(mktemp -d -t kpt-XXXX)
   local localpkg="$temp/$name"
   kpt pkg get --for-deployment "https://github.com/nephio-project/nephio-example-packages.git/$pkg" "$localpkg"
-  # sudo because docker
-  sudo kpt fn render "$localpkg"
+  kpt fn render "$localpkg"
   kpt live init "$localpkg"
   kubectl --kubeconfig "$HOME/.kube/config" api-resources
   kpt pkg tree "$localpkg"
