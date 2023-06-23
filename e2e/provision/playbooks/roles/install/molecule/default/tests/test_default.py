3ca0bf7da7d9a37a4b00d3533001db8130fdf161
Victor Morales <v.morales@samsung.com>
2023-06-22 23:06:52 -0700

    Fix install molecule test (#106)
    * Fix install molecule test
    
    Signed-off-by: Victor Morales <v.morales@samsung.com>
    
    * Improve install role documentation
    
    Signed-off-by: Victor Morales <v.morales@samsung.com>
    
    * Improve system tests for installation role
    
    Signed-off-by: Victor Morales <v.morales@samsung.com>
    
    * Expand example of bootstrap role doc
    
    Signed-off-by: Victor Morales <v.morales@samsung.com>
    
    * Validate api-resources in bootstrap system test
    
    Signed-off-by: Victor Morales <v.morales@samsung.com>
    
    ---------
    
    Signed-off-by: Victor Morales <v.morales@samsung.com>

--- e2e/provision/playbooks/roles/install/molecule/default/tests/test_default.py
+++ e2e/provision/playbooks/roles/install/molecule/default/tests/test_default.py
@@ -14,32 +14,112 @@
 #
 
 
-def test_deployments(host):
+def _check_k8s_deployment(host, deployment, namespace):
+    assert host.run(
+        "kubectl get deployments -n %s %s" % (namespace, deployment)
+    ).succeeded
+    assert host.run(
+        "kubectl rollout status deployment -n %s %s" % (namespace, deployment)
+    ).succeeded
+
+
+def test_config_management_deployments_creation(host):
     expected = {
         "config-management-monitoring": ["otel-collector"],
         "config-management-system": [
             "config-management-operator",
             "reconciler-manager",
         ],
+    }
+    for namespace, deployments in expected.items():
+        for deploy in deployments:
+            _check_k8s_deployment(host, deploy, namespace)
+
+
+def test_porch_deployments_creation(host):
+    expected = {
         "porch-system": [
             "function-runner",
             "porch-controllers",
             "porch-server",
         ],
-        "nephio-system": [
-            "ipam-controller",
-            "nephio-5gc-controller",
-            "nf-injector-controller",
-            "package-deployment-controller-controller",
-        ],
-        "nephio-webui": ["nephio-webui"],
-        "resource-group-system": ["resource-group-controller-manager"],
     }
-    got = host.check_output(
-        'kubectl get deploy -A \
--o jsonpath=\'{range .items[*]}{.metadata.name}{" "}{.metadata.namespace}\
-{"\\n"}{end}\''
-    )
     for namespace, deployments in expected.items():
         for deploy in deployments:
-            assert deploy + " " + namespace in got
+            _check_k8s_deployment(host, deploy, namespace)
+
+
+def test_nephio_webui_deployments_creation(host):
+    _check_k8s_deployment(host, "nephio-webui", "nephio-webui")
+
+
+def test_nephio_system_deployments_creation(host):
+    _check_k8s_deployment(host, "nephio-controller", "nephio-system")
+
+
+def test_resource_group_system_deployments_creation(host):
+    _check_k8s_deployment(
+        host, "resource-group-controller-manager", "resource-group-system"
+    )
+
+
+def test_repositories_creation(host):
+    expected = ["free5gc-packages", "nephio-example-packages"]
+    got = host.check_output(
+        "kubectl get repositories -o \
+jsonpath='{range .items[*]}{.status.conditions[*].ready.true}{.metadata.name}{\"\\n\"}{end}'"
+    )
+    for repository in expected:
+        assert repository in got
+
+
+def _check_api_resources(host, expected):
+    for group, resources in expected.items():
+        got = host.check_output(
+            "kubectl api-resources --output=name --api-group=" + group
+        )
+        for resource in resources:
+            assert resource + "." + group in got
+
+
+def test_api_resources_created_for_nephio(host):
+    expected = {
+        "config.nephio.org": ["networks"],
+        "infra.nephio.org": [
+            "clustercontexts",
+            "networkconfigs",
+            "repositories",
+            "tokens",
+            "workloadclusters",
+        ],
+        "req.nephio.org": [
+            "capacities",
+            "datanetworknames",
+            "datanetworks",
+            "interfaces",
+        ],
+        "workload.nephio.org": [
+            "amfdeployments",
+            "smfdeployments",
+            "upfdeployments",
+        ],
+    }
+    _check_api_resources(host, expected)
+
+
+def test_api_resources_created_for_porch(host):
+    expected = {
+        "config.porch.kpt.dev": [
+            "packagerevs",
+            "packagevariants",
+            "packagevariantsets",
+            "repositories",
+        ],
+        "porch.kpt.dev": [
+            "functions",
+            "packagerevisionresources",
+            "packagerevisions",
+            "packages",
+        ],
+    }
+    _check_api_resources(host, expected)
