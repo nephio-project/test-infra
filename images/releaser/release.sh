92fdac961b43bb4034885c963127c4f55d1bb181
Rado Chmiel <r.chmiel@partner.samsung.com>
2023-07-04 22:44:41 +0200

    Update images/releaser/release.sh
    Co-authored-by: Victor Morales <v.morales@samsung.com>

--- images/releaser/release.sh
+++ images/releaser/release.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2023 The Nephio Authors.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
 digest_raw=$(crane digest "$1")
 digest=$(echo "${digest_raw//:/-}")
 echo "Image digest: $digest_raw"
