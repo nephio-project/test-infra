24d7ed38ec06c7ef743b93c4109c2a38a0d22185
Rado Chmiel <r.chmiel@partner.samsung.com>
2023-07-05 00:50:52 +0200

    Releaser image (#134)
    * Releaser image
    
    This will be image to support release process (sbom generation, tagging and uploading images)
    
    * Create release.sh
    
    * Update .prow.yaml
    
    * Update release.sh
    
    * Update images/releaser/Dockerfile
    
    Co-authored-by: Victor Morales <v.morales@samsung.com>
    
    * Update images/releaser/release.sh
    
    Co-authored-by: Victor Morales <v.morales@samsung.com>
    
    * Update images/releaser/release.sh
    
    Co-authored-by: Victor Morales <v.morales@samsung.com>
    
    ---------
    
    Co-authored-by: Victor Morales <v.morales@samsung.com>

---
+++ images/releaser/release.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2023 The Nephio Authors.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+digest_raw=$(crane digest "$1")
+digest=$(echo "${digest_raw//:/-}")
+echo "Image digest: $digest_raw"
+echo $digest_raw > $ARTIFACTS/trivy.log
+trivy image "$1" >> $ARTIFACTS/trivy.log 2>&1
+echo $digest_raw > $ARTIFACTS/grype.log
+grype -v "$1" >> $ARTIFACTS/grype.log 2>&1
+syft "$1" --output=spdx-json >> $ARTIFACTS/$digest.sbom
+repo=$(echo "$1" | awk -F':' '{print $1}')
+echo $repo
+
+
+oras tag docker.io/"$1" $digest
+
+oras push --artifact-type sbom/example docker.io/$repo:$digest.sbom $digest.sbom
