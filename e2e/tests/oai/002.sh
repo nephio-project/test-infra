ff01c960712406514346de406e851b568b2805b4
Victor Morales <v.morales@samsung.com>
2023-12-07 15:36:04 -0800

    Add second OAI e2e test
    Signed-off-by: Victor Morales <v.morales@samsung.com>

---
+++ e2e/tests/oai/002.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#!/usr/bin/env bash
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2023 The Nephio Authors.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+## TEST METADATA
+## TEST-NAME: Deploy OAI Core Operators
+##
+
+set -o pipefail
+set -o errexit
+set -o nounset
+[[ ${DEBUG:-false} != "true" ]] || set -o xtrace
+
+# shellcheck source=e2e/defaults.env
+source "$E2EDIR/defaults.env"
+
+# shellcheck source=e2e/lib/k8s.sh
+source "${LIBDIR}/k8s.sh"
+
+# shellcheck source=e2e/lib/kpt.sh
+source "${LIBDIR}/kpt.sh"
+
+k8s_apply "$TESTDIR/002-oai-operators.yaml"
+
+for pkgvar in common-regional-database cp-operators up-operators; do
+    k8s_wait_ready "packagevariant" "oai-$pkgvar"
+done
+kpt_wait_pkg "regional" "database"
+kpt_wait_pkg "regional" "oai-cp-operators"
+kpt_wait_pkg "edge" "oai-up-operators"
+
+_regional_kubeconfig="$(k8s_get_capi_kubeconfig "regional")"
+k8s_wait_ready_replicas "deployment" "mysql" "$_regional_kubeconfig" "oai-core"
+for controller in amf ausf nrf smf udm udr; do
+    k8s_wait_ready_replicas "deployment" "oai-$controller-controller" "$_regional_kubeconfig" "oai-operators"
+done
+k8s_wait_ready_replicas "deployment" "oai-upf-controller" "$(k8s_get_capi_kubeconfig "edge")" "oai-operators"
