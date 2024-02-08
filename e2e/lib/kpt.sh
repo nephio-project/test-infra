550ba6f81512c4ff2793574ab19717a80c83bd64
Victor Morales <v.morales@samsung.com>
2024-02-08 09:00:27 -0800

    Reduce log entries
    Signed-off-by: Victor Morales <v.morales@samsung.com>

--- e2e/lib/kpt.sh
+++ e2e/lib/kpt.sh
@@ -25,24 +25,22 @@ function _wait_for_user_repo {
     local repo=$1
     local user=${2:-nephio}
     local timeout=${3:-600}
+    lapse=$timeout
 
     info "looking for $repo repository of $user user"
     local found="False"
-    while [[ $timeout -gt 0 ]]; do
+    while [[ $lapse -gt 0 ]]; do
         found=$(curl_gitea_api "repos/$user/$repo" 'import json; import sys; print("full_name" in json.loads(sys.stdin.read()))')
         if [[ $found == "True" ]]; then
-            debug "timeout: $timeout"
-            break
+            [ $((timeout * 2 / 3)) -lt $lapse ] || warn "$user user took $lapse seconds to exist in $repo repository"
+            return
         fi
-        timeout=$((timeout - 5))
+        lapse=$((lapse - 5))
         sleep 5
     done
 
-    if [[ $found != "True" ]]; then
-        curl_gitea_api "users/$user/repos" 'import json; import sys; print("\n".join(repo["full_name"] for repo in json.loads(sys.stdin.read())))'
-        error "Timed out waiting for $repo repository"
-    fi
-    info "Found $user/$repo repository"
+    curl_gitea_api "users/$user/repos" 'import json; import sys; print("\n".join(repo["full_name"] for repo in json.loads(sys.stdin.read())))'
+    error "Timed out waiting for $repo repository"
 }
 
 # kpt_wait_pkg() - Wait for a given kpt package to exist in a given repository
@@ -51,23 +49,22 @@ function kpt_wait_pkg {
     local pkg=$2
     local user=${3:-nephio}
     local timeout=${4:-600}
+    lapse=$timeout
 
     _wait_for_user_repo "$repo" "$user"
     info "looking for $pkg kpt package on $user/$repo repository"
     local found="False"
-    while [[ $timeout -gt 0 ]]; do
+    while [[ $lapse -gt 0 ]]; do
         found=$(curl_gitea_api "repos/$user/$repo/contents" "import json; import sys; print('$pkg' in [dir['path'] for dir in json.loads(sys.stdin.read()) if dir['type'] == 'dir' ])")
         if [[ $found == "True" ]]; then
-            debug "timeout: $timeout"
-            break
+            [ $((timeout * 2 / 3)) -lt $lapse ] || warn "$pkg kpt package took $lapse seconds to exist"
+            return
         fi
-        timeout=$((timeout - 5))
+        lapse=$((lapse - 5))
         sleep 5
     done
 
-    if [[ $found != "True" ]]; then
-        curl_gitea_api "repos/$user/$repo/contents" 'import json; import sys; print("\n".join(dir["path"] for dir in json.loads(sys.stdin.read()) if dir["type"] == "dir" ))'
-        error "Timed out waiting for $pkg kpt package"
-    fi
-    info "Found $pkg kpt package in $user/$repo repository"
+    porchctl rpkg get --name "$pkg"
+    curl_gitea_api "repos/$user/$repo/contents" 'import json; import sys; print("\n".join(dir["path"] for dir in json.loads(sys.stdin.read()) if dir["type"] == "dir" ))'
+    error "Timed out waiting for $pkg kpt package"
 }
--- e2e/lib/porch.sh
+++ e2e/lib/porch.sh
@@ -15,24 +15,22 @@ source "${E2EDIR:-$HOME/test-infra/e2e}/lib/_utils.sh"
 function porch_wait_log_entry {
     local pattern="$1"
     local timeout=${2:-600}
+    lapse=$timeout
 
     info "looking for $pattern log entry in porch server($(kubectl get pods -n porch-system -l app=porch-server -o jsonpath='{.items[*].metadata.name}'))"
     local found=""
-    while [[ $timeout -gt 0 ]]; do
-        found=$(kubectl logs -n porch-system "$(kubectl get pods -n porch-system -l app=porch-server -o jsonpath='{.items[*].metadata.name}')" | { grep "$pattern" || :; })
+    while [[ $lapse -gt 0 ]]; do
+        found=$(kubectl logs -n porch-system -l app=porch-server --tail -1 | { grep "$pattern" || :; })
         if [[ $found ]]; then
-            debug "timeout: $timeout"
-            break
+            [ $((timeout * 2 / 3)) -lt $lapse ] || warn "$pattern took $lapse seconds to be found in the log"
+            return
         fi
-        timeout=$((timeout - 5))
+        lapse=$((lapse - 5))
         sleep 5
     done
 
-    if [[ -z $found ]]; then
-        kubectl logs -n porch-system "$(kubectl get pods -n porch-system -l app=porch-server -o jsonpath='{.items[*].metadata.name}')"
-        error "Timed out waiting for $pattern"
-    fi
-    info "Found $pattern log entry in porch server"
+    kubectl logs -n porch-system -l app=porch-server --tail -1
+    error "Timed out waiting for $pattern"
 }
 
 # porch_wait_published_packagerev() - Waits for a kpt package revision gets published
@@ -41,10 +39,11 @@ function porch_wait_published_packagerev {
     local repository="$2"
     local revision="${3:-main}"
     local timeout=${4:-900}
+    lapse=$timeout
 
     info "looking for package published revision on $pkg_name"
     local found=""
-    while [[ $timeout -gt 0 ]]; do
+    while [[ $lapse -gt 0 ]]; do
         for pkg_rev in $(kubectl get packagerevisions -o jsonpath="{range .items[?(@.spec.packageName==\"$pkg_name\")]}{.metadata.name}{\"\\n\"}{end}"); do
             if [ "$(kubectl get packagerevision "$pkg_rev" -o jsonpath='{.spec.repository}/{.spec.revision}')" == "$repository/$revision" ]; then
                 found=$pkg_rev
@@ -52,10 +51,10 @@ function porch_wait_published_packagerev {
             fi
         done
         if [[ $found ]]; then
-            debug "timeout: $timeout"
+            [ $((timeout * 2 / 3)) -lt $lapse ] || warn "$pkg_name package took $lapse seconds to be published"
             break
         fi
-        timeout=$((timeout - 5))
+        lapse=$((lapse - 5))
         sleep 5
     done
 
