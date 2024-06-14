73072b0f7f10584e5b8d6c6ed94069660f2cb944
Anurag Rajawat <anuragsinghrajawat22@gmail.com>
2024-06-14 22:08:13 +0530

    feat: Automate released image signing (#266)
    Signed-off-by: Anurag Rajawat <anuragsinghrajawat22@gmail.com>

--- images/releaser/release.sh
+++ images/releaser/release.sh
@@ -8,16 +8,5 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 digest_raw=$(crane digest "$1")
-digest=$(echo "${digest_raw//:/-}")
-echo "Image digest: $digest_raw"
-echo $digest_raw >$ARTIFACTS/trivy.log
-trivy image "$1" >>$ARTIFACTS/trivy.log 2>&1
-echo $digest_raw >$ARTIFACTS/grype.log
-grype -v "$1" >>$ARTIFACTS/grype.log 2>&1
-syft "$1" --output=spdx-json >>$ARTIFACTS/$digest.sbom
-repo=$(echo "$1" | awk -F':' '{print $1}')
-echo $repo
-
-oras tag docker.io/"$1" $digest
-
-oras push --artifact-type sbom/example docker.io/$repo:$digest.sbom $digest.sbom
+echo "Signing \"$1@${digest_raw}\" image"
+cosign sign -y --key env://COSIGN_PRIVATE_KEY "$1@${digest_raw}"
