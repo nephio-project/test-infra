8d9d67dfe172c50b92ae2e1642d640a98ec5909e
Victor Morales <chipahuac@hotmail.com>
2024-06-20 00:30:39 -0700

    Fix Ansible linting issues (#271)
    * Fix linting ansible issues
    
    * Add disk usage information

--- e2e/provision/init.sh
+++ e2e/provision/init.sh
@@ -27,6 +27,8 @@ function get_status {
         printf "Memory free(Kb):"
         awk -v low="$(grep low /proc/zoneinfo | awk '{k+=$2}END{print k}')" '{a[$1]=$2}  END{ print a["MemFree:"]+a["Active(file):"]+a["Inactive(file):"]+a["SReclaimable:"]-(12*low);}' /proc/meminfo
     fi
+    printf "Disk usage: "
+    sudo df -h
     if command -v docker >/dev/null; then
         echo "Docker statistics:"
         docker stats --no-stream
