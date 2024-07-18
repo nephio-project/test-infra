28909986e0a36c2a86139327c5be1924dff04b63
Liam Fallon <35595825+liamfallon@users.noreply.github.com>
2024-07-18 17:12:25 +0100

    Add flags to allow single e2e tests to run (#285)

--- e2e/e2e.sh
+++ e2e/e2e.sh
@@ -13,6 +13,18 @@ set -o errexit
 set -o nounset
 [[ ${DEBUG:-false} != "true" ]] || set -o xtrace
 
+start_at_test=""
+finish_before_test=""
+
+while getopts s:f: flag
+do
+    case "${flag}" in
+        s) start_at_test=${OPTARG};;
+        f) finish_before_test=${OPTARG};;
+        *) ;;
+    esac
+done
+
 export E2EDIR=${E2EDIR:-$HOME/test-infra/e2e}
 
 # shellcheck source=e2e/defaults.env
@@ -24,7 +36,25 @@ source "$LIBDIR/testing.sh"
 failed=$((0))
 test_summary=""
 sudo dmesg >/tmp/e2e_dmesg_base.log
-for t in $TESTDIR/*.sh; do
+for t in "$TESTDIR"/*.sh; do
+    if [ -n "$start_at_test" ]
+    then
+        if [ "$t" == "$start_at_test" ]
+        then
+            start_at_test=""
+        else
+            continue
+        fi
+    fi
+
+    if [ -n "$finish_before_test" ]
+    then
+        if [ "$t" == "$finish_before_test" ]
+        then
+            break
+        fi
+    fi
+
     if ! run_test "$t"; then
         failed=$((failed + 1))
         [[ ${FAIL_FAST:-false} != "true" ]] || break
