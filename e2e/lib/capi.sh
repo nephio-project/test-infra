5d5ad3edbfb84abb4aacc3550799599e2595a8b3
Fiachra Corcoran <fiachra.corcoran@est.tech>
2025-02-05 07:06:53 +0000

    Add fluxcd workload suite (#320)
    * Add centralised fluxcd workload cluster deploy suite
    
    * Add new prow jobs for flux suite
    
    * Update gen-kust-res image

--- e2e/lib/capi.sh
+++ e2e/lib/capi.sh
@@ -2,7 +2,7 @@
 #!/usr/bin/env bash
 # SPDX-license-identifier: Apache-2.0
 ##############################################################################
-# Copyright (c) 2023 The Nephio Authors.
+# Copyright (c) 2023, 2025 The Nephio Authors.
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Apache License, Version 2.0
 # which accompanies this distribution, and is available at
@@ -17,7 +17,8 @@ source "${E2EDIR:-$HOME/test-infra/e2e}/lib/kpt.sh"
 # capi_cluster_ready() - Wait for Cluster API cluster service readiness
 function capi_cluster_ready {
     local cluster=$1
-    local kubeconfig=${2:-"$HOME/.kube/config"}
+    local use_configsync=${2:-true}
+    local kubeconfig=${3:-"$HOME/.kube/config"}
 
     k8s_wait_ready "cl" "$cluster"
     for machineset in $(kubectl get machineset -l cluster.x-k8s.io/cluster-name="$cluster" -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' --kubeconfig "$kubeconfig"); do
@@ -25,9 +26,16 @@ function capi_cluster_ready {
     done
 
     # Wait for package variants
-    for pv in cluster configsync kindnet local-path-provisioner multus repo rootsync vlanindex; do
+    for pv in cluster kindnet local-path-provisioner multus repo vlanindex; do
         k8s_wait_exists "packagevariants" "${cluster}-$pv"
+       
     done
+
+    if [[ "$use_configsync" = true ]] ; then
+        for pv in configsync rootsync; do
+            k8s_wait_exists "packagevariants" "${cluster}-$pv"
+        done
+    fi
 
     # Wait for package exists
     _wait_for_user_repo "$cluster"
@@ -38,16 +46,26 @@ function capi_cluster_ready {
     done
 
     # Wait for management staging packages exist
-    for pkg in configsync kindnet local-path-provisioner multus rootsync; do
+    for pkg in kindnet local-path-provisioner multus; do
         kpt_wait_pkg "mgmt-staging" "${cluster}-$pkg"
     done
 
+    if [[ "$use_configsync" = true ]] ; then
+        for pkg in configsync rootsync; do
+            kpt_wait_pkg "mgmt-staging" "${cluster}-$pkg"
+        done
+    fi
+
     # Wait for deployments and daemonsets readiness
     kubeconfig=$(k8s_get_capi_kubeconfig "$cluster")
-    k8s_wait_ready_replicas "deployment" "otel-collector" "$kubeconfig" "config-management-monitoring"
-    for deploy in config-management-operator reconciler-manager "root-reconciler-$cluster"; do
-        k8s_wait_ready_replicas "deployment" "$deploy" "$kubeconfig" "config-management-system"
-    done
+
+    if [[ "$use_configsync" = true ]] ; then
+        k8s_wait_ready_replicas "deployment" "otel-collector" "$kubeconfig" "config-management-monitoring"
+        for deploy in config-management-operator reconciler-manager "root-reconciler-$cluster"; do
+            k8s_wait_ready_replicas "deployment" "$deploy" "$kubeconfig" "config-management-system"
+        done
+    fi
+    
     k8s_wait_ready_replicas "deployment" "local-path-provisioner" "$kubeconfig" "local-path-storage"
     k8s_wait_ready_replicas "daemonset" "kindnet" "$kubeconfig" "kube-system"
     k8s_wait_ready_replicas "daemonset" "kube-multus-ds" "$kubeconfig" "kube-system"
